8538c6b96bca851a5dfd31d387c304b922286d19
ml-at-koumbit <info@koumbit.org>
2023-12-28 14:16:54 -0500

    Fixes the disappearing DMARC issue
    The issue was missing parentheses in a SQL request, which
    deleted SPF for the domain, but DMARC from all domains. This
    was resolved in splitting the complex SQL query into two queries.

--- bureau/class/m_mail.php
+++ bureau/class/m_mail.php
@@ -485,7 +485,8 @@ ORDER BY
         }
         $db->query("SELECT domaine FROM domaines WHERE id= ? ;", array($dom_id));
         if ($db->next_record()) {
-            $db->query("UPDATE sub_domaines SET web_action='DELETE' WHERE domaine= ? AND type='txt' AND (sub='' AND valeur LIKE 'v=spf1 %') OR (sub='_dmarc' AND valeur LIKE 'v=dmarc1;%');", array($db->Record["domaine"]));
+            $db->query("UPDATE sub_domaines SET web_action='DELETE' WHERE domaine= ? AND type='txt' AND sub='' AND valeur LIKE 'v=spf1 %';", array($db->Record["domaine"])); 
+            $db->query("UPDATE sub_domaines SET web_action='DELETE' WHERE domaine= ? AND type='txt' AND sub='_dmarc' AND valeur LIKE 'v=dmarc1;%';", array($db->Record["domaine"]));
             $db->query("UPDATE sub_domaines SET web_action='DELETE' WHERE domaine= ? AND (type='defmx' OR type='defmx2');", array($db->Record["domaine"]));
             $db->query("UPDATE domaines SET dns_action='UPDATE' WHERE id= ? ;", array($dom_id));
         }
